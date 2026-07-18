dbbed43ff05e8c5536faa6875aedaab4a7e9481b
Md. Misskhat Hossen Milon <misskhat.hossen.com>
2026-07-19 01:32:40 +0600

    project setup for revision of react router & react

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,7 @@
-import "./App.css";
-
-//CREATE A VARIABLE AND RENDER INSIDE JSX
-const myElement = (
-  <table>
-    <tr>
-      <th>Name</th>
-    </tr>
-    <tr>
-      <td>Mishkat</td>
-    </tr>
-    <tr>
-      <td>Sumayia</td>
-    </tr>
-  </table>
-);
-
-// REACT ES6 MOST IMPORT FOR LEARNING REACT
-/**
- * class
- * Arrow function
- * Variable (let, const)
- * Array method => most important (map, forEach, filter, find, reduce)
- * Destructuring
- * Modules
- * Ternary operator
- * Spread operator
- */
-
-class Car {
-  constructor(name, year) {
-    this.brandName = name;
-    this.launchYear = year;
-  }
-
-  about() {
-    return `I have ${this.brandName} car and it is launch ${this.launchYear}`;
-  }
-}
-const myCar = new Car("BMW", 2027);
-
-const fruits = ["apple", "banana", "cherry"];
-
 function App() {
   return (
     <div>
-      {/* RENDER VARIABLE */}
-      <div>{myElement}</div>
-
-      {/* RENDER NEW CLASS INSIDE METHOD */}
-      <div>{myCar.about()}</div>
-
-      {/* RENDER ARRAY IN MAP */}
-      <ol>
-        {fruits.map((fruit, idx) => (
-          <li key={idx}>{fruit}</li>
-        ))}
-      </ol>
+      <button class="btn btn-primary">Button</button>
     </div>
   );
 }
