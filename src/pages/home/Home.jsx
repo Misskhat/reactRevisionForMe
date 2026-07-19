7f816bbf4f900abb6038b5c7fbef9c740dd5b1f2
Md. Misskhat Hossen Milon <misskhat.hossen.com>
2026-07-19 18:56:09 +0600

    create custom hook for data loading

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import useProducts from "../../hooks/useProducts";
 
 function Home() {
+  const data = useProducts();
+  console.log(data);
   return <div>Home</div>;
 }
 
